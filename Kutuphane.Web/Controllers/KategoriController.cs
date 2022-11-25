fa3b7bf4503c4f317523b0c3a5f3022f86451769
Bilal Beşir Sarıkaya <bbesir@hotmail.com>
2022-11-25 19:46:59 +0300

    test

---
+++ Kutuphane.Web/Controllers/KategoriController.cs
@@ -0,0 +1,23 @@
+﻿using Kutuphane.Web.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kutuphane.Web.Controllers
+{
+    public class KategoriController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public KategoriController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GetAll()
+        {
+            return Json(_db.Kategoriler.ToList());
+        }
+    }
+}
